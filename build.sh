946581879061ab575b1c83cc722a7ca429356baf
Shuo Chen <chenshuo@chenshuo.com>
2010-03-13 12:09:47 +0000

    Build both types.

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
+set -x
+
 SOURCE_DIR=`pwd`
 BUILD_DIR=${BUILD_DIR:-../build}
-mkdir -p $BUILD_DIR
-cd $BUILD_DIR && cmake $SOURCE_DIR && make
+BUILD_TYPE=${BUILD_TYPE:-Debug}
+
+mkdir -p $BUILD_DIR/$BUILD_TYPE \
+  && cd $BUILD_DIR/$BUILD_TYPE \
+  && cmake --graphviz=dep.dot -DCMAKE_BUILD_TYPE=$BUILD_TYPE $SOURCE_DIR \
+  && make
+
+cd $SOURCE_DIR && doxygen
 
