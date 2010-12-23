5614a11d22a012c99ecf061cccce91f3a123e235
Shuo Chen <chenshuo@chenshuo.com>
2010-12-23 10:47:09 +0800

    atfork() for resetting t_cachedTid in child process.

--- muduo/base/Thread.cc
+++ muduo/base/Thread.cc
@@ -27,12 +27,20 @@ pid_t gettid()
   return static_cast<pid_t>(::syscall(SYS_gettid));
 }
 
+void afterFork()
+{
+  t_cachedTid = gettid();
+  muduo::CurrentThread::t_threadName = "main";
+  // no need to call pthread_atfork(NULL, NULL, &afterFork);
+}
+
 class ThreadNameInitializer
 {
  public:
   ThreadNameInitializer()
   {
     muduo::CurrentThread::t_threadName = "main";
+    pthread_atfork(NULL, NULL, &afterFork);
   }
 };
 
