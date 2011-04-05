3a9007f6a8483d334e814e438e8ae69ea5b2e6ac
Shuo Chen <chenshuo@chenshuo.com>
2011-04-05 20:54:53 +0800

    Revert "Add max connection limit to simple echo example."
    This reverts commit b157cca76c274b9656a622e26809f27fb343d8d4.

--- examples/simple/echo/echo.cc
+++ examples/simple/echo/echo.cc
@@ -7,8 +7,6 @@
 using namespace muduo;
 using namespace muduo::net;
 
-const int kMaxConnections = 1024*1024;
-
 EchoServer::EchoServer(EventLoop* loop,
                        const InetAddress& listenAddr)
   : loop_(loop),
@@ -25,25 +23,11 @@ void EchoServer::start()
   server_.start();
 }
 
-int numConnected = 0;
 void EchoServer::onConnection(const TcpConnectionPtr& conn)
 {
   LOG_INFO << "EchoServer - " << conn->peerAddress().toHostPort() << " -> "
     << conn->localAddress().toHostPort() << " is "
     << (conn->connected() ? "UP" : "DOWN");
-  if (conn->connected())
-  {
-    ++numConnected;
-    if (numConnected > kMaxConnections)
-    {
-      conn->shutdown();
-    }
-  }
-  else
-  {
-    --numConnected;
-  }
-  LOG_INFO << "numConnected = " << numConnected;
 }
 
 void EchoServer::onMessage(const TcpConnectionPtr& conn,
