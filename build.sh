4685322664237d14b640fba47598a9cbb20703b9
Shuo Chen <chenshuo@chenshuo.com>
2011-05-24 21:15:25 +0800

    add cmake install

--- build.sh
+++ build.sh
@@ -5,10 +5,11 @@ set -x
 SOURCE_DIR=`pwd`
 BUILD_DIR=${BUILD_DIR:-../build}
 BUILD_TYPE=${BUILD_TYPE:-debug}
+INSTALL_DIR=${INSTALL_DIR:-../${BUILD_TYPE}-install}
 
 mkdir -p $BUILD_DIR/$BUILD_TYPE \
   && cd $BUILD_DIR/$BUILD_TYPE \
-  && cmake --graphviz=dep.dot -DCMAKE_BUILD_TYPE=$BUILD_TYPE $SOURCE_DIR \
+  && cmake --graphviz=dep.dot -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR $SOURCE_DIR \
   && make $*
 
 cd $SOURCE_DIR && doxygen
