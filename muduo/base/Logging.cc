61ba677664e0533b63b53c5eec93b9eab862b5aa
Shuo Chen <chenshuo@chenshuo.com>
2012-03-09 22:43:31 +0800

    Set default log level to INFO.

--- muduo/base/Logging.cc
+++ muduo/base/Logging.cc
@@ -40,8 +40,10 @@ Logger::LogLevel initLogLevel()
 {
   if (::getenv("MUDUO_LOG_TRACE"))
     return Logger::TRACE;
-  else
+  else if (::getenv("MUDUO_LOG_DEBUG"))
     return Logger::DEBUG;
+  else
+    return Logger::INFO;
 }
 
 Logger::LogLevel g_logLevel = initLogLevel();
