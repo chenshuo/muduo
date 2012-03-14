6c4d3e465ef51795a0ee262ab577ded83c2d405c
Shuo Chen <chenshuo@chenshuo.com>
2012-03-14 22:16:15 +0800

    push/pop are only supported by GCC 4.6+

--- muduo/net/Endian.h
+++ muduo/net/Endian.h
@@ -23,7 +23,9 @@ namespace sockets
 
 // the inline assembler code makes type blur,
 // so we disable warnings for a while.
+#if __GNUC_MINOR__ >= 6
 #pragma GCC diagnostic push
+#endif
 #pragma GCC diagnostic ignored "-Wconversion"
 #pragma GCC diagnostic ignored "-Wold-style-cast"
 inline uint64_t hostToNetwork64(uint64_t host64)
@@ -55,7 +57,13 @@ inline uint16_t networkToHost16(uint16_t net16)
 {
   return be16toh(net16);
 }
+#if __GNUC_MINOR__ >= 6
 #pragma GCC diagnostic pop
+#else
+#pragma GCC diagnostic error "-Wconversion"
+#pragma GCC diagnostic error "-Wold-style-cast"
+#endif
+
 
 }
 }
