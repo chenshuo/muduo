53a77083c0083f67420d9cf3bde6601d41c2d655
Shuo Chen <chenshuo@chenshuo.com>
2012-04-21 00:32:16 +0800

    fix build on 32-bit.

--- muduo/base/tests/FileUtil_test.cc
+++ muduo/base/tests/FileUtil_test.cc
@@ -1,6 +1,8 @@
 #include <muduo/base/FileUtil.h>
 
 #include <stdio.h>
+#define __STDC_FORMAT_MACROS
+#include <inttypes.h>
 
 using namespace muduo;
 
@@ -9,14 +11,14 @@ int main()
   string result;
   int64_t size = 0;
   int err = FileUtil::readFile("/proc/self", 1024, &result, &size);
-  printf("%d %zd %zd\n", err, result.size(), size);
+  printf("%d %zd %" PRIu64 "\n", err, result.size(), size);
   err = FileUtil::readFile("/proc/self/cmdline", 1024, &result, &size);
-  printf("%d %zd %zd\n", err, result.size(), size);
+  printf("%d %zd %" PRIu64 "\n", err, result.size(), size);
   err = FileUtil::readFile("/dev/null", 1024, &result, &size);
-  printf("%d %zd %zd\n", err, result.size(), size);
+  printf("%d %zd %" PRIu64 "\n", err, result.size(), size);
   err = FileUtil::readFile("/dev/zero", 1024, &result, &size);
-  printf("%d %zd %zd\n", err, result.size(), size);
+  printf("%d %zd %" PRIu64 "\n", err, result.size(), size);
   err = FileUtil::readFile("/notexist", 1024, &result, &size);
-  printf("%d %zd %zd\n", err, result.size(), size);
+  printf("%d %zd %" PRIu64 "\n", err, result.size(), size);
 }
 
