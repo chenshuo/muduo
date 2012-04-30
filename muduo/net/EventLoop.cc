chenshuo/muduo

commit e70375578f62608762f5307c85313d015066f3a2

misc changes for zurg.

muduo/net/EventLoop.cc
@@ -188,6 +188,11 @@ void EventLoop::removeChannel(Channel* channel)
 {
   assert(channel->ownerLoop() == this);
   assertInLoopThread();
+  if (eventHandling_)
+  {
+    assert(std::find(activeChannels_.begin(), activeChannels_.end(), channel)
+           == activeChannels_.end());
+  }
   poller_->removeChannel(channel);
 }
 

muduo/net/EventLoop.h
@@ -106,6 +106,7 @@ class EventLoop : boost::noncopyable
   }
   bool isInLoopThread() const { return threadId_ == CurrentThread::tid(); }
   // bool callingPendingFunctors() const { return callingPendingFunctors_; }
+  bool eventHandling() const { return eventHandling_; }
 
  private:
   void abortNotInLoopThread();

muduo/base/CMakeLists.txt
@@ -17,4 +17,6 @@ install(TARGETS muduo_base DESTINATION lib)
 file(GLOB HEADERS "*.h")
 install(FILES ${HEADERS} DESTINATION include/muduo/base)
 
-add_subdirectory(tests)
+if(NOT CMAKE_BUILD_NO_EXAMPLES)
+  add_subdirectory(tests)
+endif()

muduo/net/CMakeLists.txt
@@ -42,7 +42,10 @@ install(FILES ${HEADERS} DESTINATION include/muduo/net)
 
 add_subdirectory(http)
 add_subdirectory(inspect)
-add_subdirectory(tests)
+
+if(NOT CMAKE_BUILD_NO_EXAMPLES)
+  add_subdirectory(tests)
+endif()
 
 if(PROTOBUF_FOUND)
   add_subdirectory(protorpc)

muduo/net/Channel.cc
@@ -27,6 +27,7 @@ Channel::Channel(EventLoop* loop, int fd__)
     events_(0),
     revents_(0),
     index_(-1),
+    logHup_(true),
     tied_(false),
     eventHandling_(false)
 {
@@ -70,7 +71,8 @@ void Channel::handleEventWithGuard(Timestamp receiveTime)
   eventHandling_ = true;
   if ((revents_ & POLLHUP) && !(revents_ & POLLIN))
   {
-    LOG_WARN << "Channel::handle_event() POLLHUP";
+    if (logHup_)
+      LOG_WARN << "Channel::handle_event() POLLHUP";
     if (closeCallback_) closeCallback_();
   }
 

muduo/net/Channel.h
@@ -74,6 +74,8 @@ class Channel : boost::noncopyable
   // for debug
   string reventsToString() const;
 
+  void doNotLogHup() { logHup_ = false; }
+
   EventLoop* ownerLoop() { return loop_; }
 
  private:
@@ -89,6 +91,7 @@ class Channel : boost::noncopyable
   int        events_;
   int        revents_;
   int        index_; // used by Poller.
+  bool       logHup_;
 
   boost::weak_ptr<void> tie_;
   bool tied_;

muduo/net/TimerId.h
@@ -26,6 +26,12 @@ class Timer;
 class TimerId : public muduo::copyable
 {
  public:
+  TimerId()
+    : timer_(NULL),
+      seq_(0)
+  {
+  }
+
   TimerId(Timer* timer, int64_t seq)
     : timer_(timer),
       seq_(seq)

commit a1bb211d2d6f45a86f79d86bccbf89a126d6d1b9

fix bug introduced in e7037557.

muduo/net/EventLoop.cc
@@ -65,7 +65,8 @@ EventLoop::EventLoop()
     poller_(Poller::newDefaultPoller(this)),
     timerQueue_(new TimerQueue(this)),
     wakeupFd_(createEventfd()),
-    wakeupChannel_(new Channel(this, wakeupFd_))
+    wakeupChannel_(new Channel(this, wakeupFd_)),
+    currentActiveChannel_(NULL)
 {
   LOG_TRACE << "EventLoop created " << this << " in thread " << threadId_;
   if (t_loopInThisThread)
@@ -110,8 +111,10 @@ void EventLoop::loop()
     for (ChannelList::iterator it = activeChannels_.begin();
         it != activeChannels_.end(); ++it)
     {
-      (*it)->handleEvent(pollReturnTime_);
+      currentActiveChannel_ = *it;
+      currentActiveChannel_->handleEvent(pollReturnTime_);
     }
+    currentActiveChannel_ = NULL;
     eventHandling_ = false;
     doPendingFunctors();
   }
@@ -190,8 +193,8 @@ void EventLoop::removeChannel(Channel* channel)
   assertInLoopThread();
   if (eventHandling_)
   {
-    assert(std::find(activeChannels_.begin(), activeChannels_.end(), channel)
-           == activeChannels_.end());
+    assert(currentActiveChannel_ == channel ||
+        std::find(activeChannels_.begin(), activeChannels_.end(), channel) == activeChannels_.end());
   }
   poller_->removeChannel(channel);
 }

muduo/net/EventLoop.h
@@ -130,6 +130,7 @@ class EventLoop : boost::noncopyable
   // we don't expose Channel to client.
   boost::scoped_ptr<Channel> wakeupChannel_;
   ChannelList activeChannels_;
+  Channel* currentActiveChannel_;
   MutexLock mutex_;
   std::vector<Functor> pendingFunctors_; // @BuardedBy mutex_
 };