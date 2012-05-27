chenshuo/muduo

commit 168dccf7925b01bba138b3c1f1604da2fca5fbac

stop() in ThreadPool::dtor.

muduo/base/ThreadPool.cc
@@ -23,6 +23,7 @@ ThreadPool::ThreadPool(const string& name)
 
 ThreadPool::~ThreadPool()
 {
+  stop();
 }
 
 void ThreadPool::start(int numThreads)

commit 8d8868e7a55cfa11c094a8efb9ee1884a9349d03

fix bug introduced in 168dccf7, only stop() in ThreadPool dtor when it hasn't stoped. Thanks to Wang Shuai.

muduo/base/ThreadPool.cc
@@ -23,7 +23,10 @@ ThreadPool::ThreadPool(const string& name)
 
 ThreadPool::~ThreadPool()
 {
-  stop();
+  if (running_)
+  {
+    stop();
+  }
 }
 
 void ThreadPool::start(int numThreads)