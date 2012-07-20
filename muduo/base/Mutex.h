chenshuo/muduo

commit 45ed25ce3b97ade59ac3d2175feb8e922a15fc94

check return value of pthread_mutex_init and pthread_mutex_destroy.

muduo/base/Mutex.h
@@ -20,13 +20,15 @@ class MutexLock : boost::noncopyable
   MutexLock()
     : holder_(0)
   {
-    pthread_mutex_init(&mutex_, NULL);
+    int ret = pthread_mutex_init(&mutex_, NULL);
+    assert(ret == 0);
   }
 
   ~MutexLock()
   {
     assert(holder_ == 0);
-    pthread_mutex_destroy(&mutex_);
+    int ret = pthread_mutex_destroy(&mutex_);
+    assert(ret = 0);
   }
 
   bool isLockedByThisThread()
@@ -67,7 +69,8 @@ class MutexLock : boost::noncopyable
 class MutexLockGuard : boost::noncopyable
 {
  public:
-  explicit MutexLockGuard(MutexLock& mutex) : mutex_(mutex)
+  explicit MutexLockGuard(MutexLock& mutex)
+    : mutex_(mutex)
   {
     mutex_.lock();
   }

muduo/base/Condition.h
@@ -17,7 +17,8 @@ namespace muduo
 class Condition : boost::noncopyable
 {
  public:
-  explicit Condition(MutexLock& mutex) : mutex_(mutex)
+  explicit Condition(MutexLock& mutex)
+    : mutex_(mutex)
   {
     pthread_cond_init(&pcond_, NULL);
   }

muduo/base/tests/BlockingQueue_bench.cc
@@ -96,6 +96,6 @@ int main(int argc, char* argv[])
   int threads = argc > 1 ? atoi(argv[1]) : 1;
 
   Bench t(threads);
-  t.run(1000);
+  t.run(10000);
   t.joinAll();
 }

commit bb2cd128d876414a291b3a9e6f3742d8d0798525

fix bug introduced in 45ed25ce.

muduo/base/Mutex.h
@@ -28,7 +28,7 @@ class MutexLock : boost::noncopyable
   {
     assert(holder_ == 0);
     int ret = pthread_mutex_destroy(&mutex_);
-    assert(ret = 0); (void) ret;
+    assert(ret == 0); (void) ret;
   }
 
   bool isLockedByThisThread()