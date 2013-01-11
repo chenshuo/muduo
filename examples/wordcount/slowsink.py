9f217126c945408ca0ede205357f230257f52765
Shuo Chen <chenshuo@chenshuo.com>
2013-01-11 11:44:22 +0800

    slowsink.py now acts as client too.

--- examples/wordcount/slowsink.py
+++ examples/wordcount/slowsink.py
@@ -2,6 +2,7 @@
 
 import os, socket, sys, time
 
+host = ''
 port = 2007
 
 if len(sys.argv) > 1:
@@ -12,16 +13,27 @@ bps = mps * 1000000
 BUFSIZE = int(bps/10) # sleep 100ms at full speed
 
 print "Mbytes/s =", mps
-print "listening on port", port
 
-listen_address = ("", port)
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind(listen_address)
-server_socket.listen(5)
+if len(sys.argv) > 3:
+	host = sys.argv[2]
+	port = int(sys.argv[3])
+	print "connecting to %s:%d" % (host, port)
+else:
+	print "listening on port", port
 
-(client_socket, client_address) = server_socket.accept()
-print "got connection from", client_address
+if host:
+	client_socket = socket.create_connection((host, port))
+	print "connected to", client_socket.getpeername()
+else:
+	listen_address = ("", port)
+	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	server_socket.bind(listen_address)
+	server_socket.listen(5)
+
+	(client_socket, client_address) = server_socket.accept()
+	print "got connection from", client_address
+
 start = time.time()
 total_size = 0
 dot = bps
