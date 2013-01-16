chenshuo/muduo

commit dd26871ca152c96b7a0d615a86cfdfc80facdfd1

fix bug of writing after disconnected. sendInLoop() could be queued and called after handleClose().

muduo/net/TcpConnection.cc
@@ -138,6 +138,12 @@ void TcpConnection::sendInLoop(const void* data, size_t len)
   loop_->assertInLoopThread();
   ssize_t nwrote = 0;
   size_t remaining = len;
+  bool error = false;
+  if (state_ != kConnected)
+  {
+    LOG_WARN << "state = " << state_ << ", give up writing";
+    return;
+  }
   // if no thing in output queue, try writing directly
   if (!channel_->isWriting() && outputBuffer_.readableBytes() == 0)
   {
@@ -156,12 +162,16 @@ void TcpConnection::sendInLoop(const void* data, size_t len)
       if (errno != EWOULDBLOCK)
       {
         LOG_SYSERR << "TcpConnection::sendInLoop";
+        if (errno == EPIPE) // FIXME: any others?
+        {
+          error = true;
+        }
       }
     }
   }
 
   assert(remaining <= len);
-  if (remaining > 0)
+  if (!error && remaining > 0)
   {
     LOG_TRACE << "I am going to write more data";
     size_t oldLen = outputBuffer_.readableBytes();
@@ -289,14 +299,15 @@ void TcpConnection::handleWrite()
   }
   else
   {
-    LOG_TRACE << "Connection is down, no more writing";
+    LOG_TRACE << "Connection fd = " << channel_->fd()
+              << " is down, no more writing";
   }
 }
 
 void TcpConnection::handleClose()
 {
   loop_->assertInLoopThread();
-  LOG_TRACE << "TcpConnection::handleClose state = " << state_;
+  LOG_TRACE << "fd = " << channel_->fd() << " state = " << state_;
   assert(state_ == kConnected || state_ == kDisconnecting);
   // we don't close fd, leave it to dtor, so we can find leaks easily.
   setState(kDisconnected);

commit 422d4967c9c68ac7acabeeaf51ee160a175530ca

Fix bug introduced in dd26871, when call shutdown() right after send() from non-IO thread, data might be incomplete.

examples/wordcount/hasher.cc
@@ -48,7 +48,7 @@ class SendThrottler : boost::noncopyable
   {
     if (buffer_.readableBytes() > 0)
     {
-      LOG_DEBUG << "send " << buffer_.readableBytes();
+      LOG_DEBUG << "send " << buffer_.readableBytes() << " bytes";
       conn_->send(&buffer_);
     }
     conn_->shutdown();
@@ -58,6 +58,7 @@ class SendThrottler : boost::noncopyable
   void send(const string& word, int64_t count)
   {
     buffer_.append(word);
+    // FIXME: use LogStream
     char buf[64];
     snprintf(buf, sizeof buf, "\t%" PRId64 "\r\n", count);
     buffer_.append(buf);
@@ -186,8 +187,10 @@ void WordCountSender::processFile(const char* filename)
 {
   LOG_INFO << "processFile " << filename;
   WordCountMap wordcounts;
+  // FIXME: use mmap to read file
   std::ifstream in(filename);
   string word;
+  // FIXME: make local hash optional.
   boost::hash<string> hash;
   while (in)
   {
@@ -201,7 +204,7 @@ void WordCountSender::processFile(const char* filename)
       }
     }
 
-    LOG_DEBUG << "send " << wordcounts.size();
+    LOG_INFO << "send " << wordcounts.size() << " records";
     for (WordCountMap::iterator it = wordcounts.begin();
          it != wordcounts.end(); ++it)
     {

muduo/net/TcpConnection.cc
@@ -139,9 +139,9 @@ void TcpConnection::sendInLoop(const void* data, size_t len)
   ssize_t nwrote = 0;
   size_t remaining = len;
   bool error = false;
-  if (state_ != kConnected)
+  if (state_ == kDisconnected)
   {
-    LOG_WARN << "state = " << state_ << ", give up writing";
+    LOG_WARN << "disconnected, give up writing";
     return;
   }
   // if no thing in output queue, try writing directly