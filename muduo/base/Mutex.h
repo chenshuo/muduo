2e9134e1de5f07b599b96392e504e41657111793
Shuo Chen <chenshuo@chenshuo.com>
2013-08-29 13:14:43 +0800

    only evaluate argument once in MCHECK macro.

--- muduo/base/Mutex.h
+++ muduo/base/Mutex.h
@@ -11,6 +11,8 @@
 #include <assert.h>
 #include <pthread.h>
 
+#ifdef CHECK_PTHREAD_RETURN_VALUE
+
 #ifdef NDEBUG
 __BEGIN_DECLS
 extern void __assert_perror_fail (int errnum,
@@ -21,8 +23,16 @@ extern void __assert_perror_fail (int errnum,
 __END_DECLS
 #endif
 
-#define MCHECK(errnum) ((errnum) == 0 ? (void) 0 : \
-                        __assert_perror_fail ((errnum), __FILE__, __LINE__, __func__))
+#define MCHECK(ret) ({ __typeof__ (ret) errnum = (ret);         \
+                       if (__builtin_expect(errnum != 0, 1))    \
+                         __assert_perror_fail (errnum, __FILE__, __LINE__, __func__);})
+
+#else  // CHECK_PTHREAD_RETURN_VALUE
+
+#define MCHECK(ret) ({ __typeof__ (ret) errnum = (ret);         \
+                       assert(errnum == 0); (void) errnum;})
+
+#endif // CHECK_PTHREAD_RETURN_VALUE
 
 namespace muduo
 {
