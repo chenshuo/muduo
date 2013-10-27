f63d59e5cd5a5d9a0340237f479d498d76283def
JnuSimba <dameng34@163.com>
2013-10-27 15:43:25 +0800

    Update Excetion.cc(demangle funcion name with abi::__cxa_demangle)
    1、打开#include <cxxabi.h> 的注释
    2、添加#include <stdio.h>
    3、void Exception::fillStackTrace()
    {
           ..........
          // TODO demangle funcion name with abi::__cxa_demangle
          //stack_.append(strings[i]);
            stack_.append(demangle(strings[i]));
         .............
    }

--- muduo/base/Exception.cc
+++ muduo/base/Exception.cc
@@ -5,9 +5,10 @@
 
 #include <muduo/base/Exception.h>
 
-//#include <cxxabi.h>
+#include <cxxabi.h>
 #include <execinfo.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 using namespace muduo;
 
@@ -37,6 +38,30 @@ const char* Exception::stackTrace() const throw()
   return stack_.c_str();
 }
 
+string Exception::demangle(const char *symbol)
+{
+    size_t size;
+    int status;
+    char temp[128] = {0};
+    char *demangled;  //first, try to demangle a c++ name
+    if (1 == sscanf(symbol, "%*[^(]%*[^_]%127[^)+]", temp))
+    {
+        if (NULL != (demangled = abi::__cxa_demangle(temp, NULL, &size, &status)))
+        {
+            string result(demangled);
+            free(demangled);
+            return result;
+        }
+    }
+    //if that didn't work, try to get a regular c symbol
+    if (1 == sscanf(symbol, "%127s", temp))
+    {
+        return temp;
+    }
+    //if all else fails, just return the symbol
+    return symbol;
+}
+
 void Exception::fillStackTrace()
 {
   const int len = 200;
@@ -48,7 +73,8 @@ void Exception::fillStackTrace()
     for (int i = 0; i < nptrs; ++i)
     {
       // TODO demangle funcion name with abi::__cxa_demangle
-      stack_.append(strings[i]);
+      //stack_.append(strings[i]);
+	    stack_.append(demangle(strings[i]));
       stack_.push_back('\n');
     }
     free(strings);
