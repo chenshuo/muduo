6d2e11cfdccb7da6ce6005cb7c81f7bb709ae899
Shuo Chen <chenshuo@chenshuo.com>
2013-12-03 11:31:45 +0800

    comment

--- muduo/base/Condition.cc
+++ muduo/base/Condition.cc
@@ -11,6 +11,7 @@
 bool muduo::Condition::waitForSeconds(int seconds)
 {
   struct timespec abstime;
+  // FIXME: use CLOCK_MONOTONIC or CLOCK_MONOTONIC_RAW to prevent time rewind.
   clock_gettime(CLOCK_REALTIME, &abstime);
   abstime.tv_sec += seconds;
   MutexLock::UnassignGuard ug(mutex_);
