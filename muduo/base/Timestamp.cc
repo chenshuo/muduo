eb5b992965ae701ff75564efe98d9550fb179d77
Shuo Chen <chenshuo@chenshuo.com>
2014-03-22 15:45:21 -0700

    add SystemInspector::overview()

--- muduo/base/Timestamp.cc
+++ muduo/base/Timestamp.cc
@@ -26,18 +26,27 @@ string Timestamp::toString() const
   return buf;
 }
 
-string Timestamp::toFormattedString() const
+string Timestamp::toFormattedString(bool showMicroseconds) const
 {
   char buf[32] = {0};
   time_t seconds = static_cast<time_t>(microSecondsSinceEpoch_ / kMicroSecondsPerSecond);
-  int microseconds = static_cast<int>(microSecondsSinceEpoch_ % kMicroSecondsPerSecond);
   struct tm tm_time;
   gmtime_r(&seconds, &tm_time);
 
-  snprintf(buf, sizeof(buf), "%4d%02d%02d %02d:%02d:%02d.%06d",
-      tm_time.tm_year + 1900, tm_time.tm_mon + 1, tm_time.tm_mday,
-      tm_time.tm_hour, tm_time.tm_min, tm_time.tm_sec,
-      microseconds);
+  if (showMicroseconds)
+  {
+    int microseconds = static_cast<int>(microSecondsSinceEpoch_ % kMicroSecondsPerSecond);
+    snprintf(buf, sizeof(buf), "%4d%02d%02d %02d:%02d:%02d.%06d",
+             tm_time.tm_year + 1900, tm_time.tm_mon + 1, tm_time.tm_mday,
+             tm_time.tm_hour, tm_time.tm_min, tm_time.tm_sec,
+             microseconds);
+  }
+  else
+  {
+    snprintf(buf, sizeof(buf), "%4d%02d%02d %02d:%02d:%02d",
+             tm_time.tm_year + 1900, tm_time.tm_mon + 1, tm_time.tm_mday,
+             tm_time.tm_hour, tm_time.tm_min, tm_time.tm_sec);
+  }
   return buf;
 }
 
