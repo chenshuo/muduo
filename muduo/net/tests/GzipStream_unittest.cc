88cbe4054cc577735f3f17dcc1202e2cbe4365ab
Shuo Chen <chenshuo@chenshuo.com>
2014-03-22 18:56:36 -0700

    update GzipOutputStream

--- muduo/net/tests/GzipStream_unittest.cc
+++ muduo/net/tests/GzipStream_unittest.cc
@@ -4,6 +4,8 @@
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
 
+#include <stdio.h>
+
 BOOST_AUTO_TEST_CASE(testGzipOutputStream)
 {
   muduo::net::Buffer output;
@@ -39,13 +41,9 @@ BOOST_AUTO_TEST_CASE(testGzipOutputStream3)
   muduo::net::Buffer output;
   muduo::net::GzipOutputStream stream(&output);
   BOOST_CHECK_EQUAL(stream.zlibErrorCode(), Z_OK);
-  for (int x = 0; x < 1024; ++x)
+  for (int i = 0; i < 1024*1024; ++i)
   {
-    for (int i = 0; i < 1024; ++i)
-    {
-      BOOST_CHECK(stream.write("01234567890123456789012345678901234567890123456789"));
-    }
-    //printf("%zd ", output.readableBytes());
+    BOOST_CHECK(stream.write("01234567890123456789012345678901234567890123456789"));
   }
   stream.finish();
   // printf("total %zd\n", output.readableBytes());
@@ -63,12 +61,29 @@ BOOST_AUTO_TEST_CASE(testGzipOutputStream4)
     input += "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_-"[rand() % 64];
   }
 
-  for (int x = 0; x < 10; ++x)
+  for (int i = 0; i < 10; ++i)
   {
     BOOST_CHECK(stream.write(input));
   }
   stream.finish();
   // printf("total %zd\n", output.readableBytes());
   BOOST_CHECK_EQUAL(stream.zlibErrorCode(), Z_STREAM_END);
-  // printf("t4 %d\n", stream.internalOutputBufferSize());
+}
+
+BOOST_AUTO_TEST_CASE(testGzipOutputStream5)
+{
+  muduo::net::Buffer output;
+  muduo::net::GzipOutputStream stream(&output);
+  BOOST_CHECK_EQUAL(stream.zlibErrorCode(), Z_OK);
+  muduo::string input(1024*1024, '_');
+  for (int i = 0; i < 64; ++i)
+  {
+    BOOST_CHECK(stream.write(input));
+  }
+  printf("bufsiz %d\n", stream.internalOutputBufferSize());
+  printf("total_in %zd\n", stream.inputBytes());
+  printf("total_out %zd\n", stream.outputBytes());
+  stream.finish();
+  printf("total %zd\n", output.readableBytes());
+  BOOST_CHECK_EQUAL(stream.zlibErrorCode(), Z_STREAM_END);
 }
