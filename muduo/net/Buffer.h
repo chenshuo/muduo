2a5e81cea30b5968e8f43216eb4fc48a2cf986c5
weizili.build17 <zieckey@gmail.com>
2014-09-01 13:46:13 +0800

    Modify Buffer constructor to : Buffer(size_t initialSize = kInitialSize)

--- muduo/net/Buffer.h
+++ muduo/net/Buffer.h
@@ -45,13 +45,23 @@ class Buffer : public muduo::copyable
   static const size_t kCheapPrepend = 8;
   static const size_t kInitialSize = 1024;
 
-  Buffer()
-    : buffer_(kCheapPrepend + kInitialSize),
+//  Buffer()
+//    : buffer_(kCheapPrepend + kInitialSize),
+//      readerIndex_(kCheapPrepend),
+//      writerIndex_(kCheapPrepend)
+//  {
+//    assert(readableBytes() == 0);
+//    assert(writableBytes() == kInitialSize);
+//    assert(prependableBytes() == kCheapPrepend);
+//  }
+
+  Buffer(size_t initialSize = kInitialSize)
+    : buffer_(kCheapPrepend + initialSize),
       readerIndex_(kCheapPrepend),
       writerIndex_(kCheapPrepend)
   {
     assert(readableBytes() == 0);
-    assert(writableBytes() == kInitialSize);
+    assert(writableBytes() == initialSize);
     assert(prependableBytes() == kCheapPrepend);
   }
 
