915ff9053723a40be1faf25e792a5cbe66b10d5b
Junlong Xie <decimalbell@gmail.com>
2014-11-24 21:59:31 +0800

    accept all incoming connections until no more

--- muduo/net/Acceptor.cc
+++ muduo/net/Acceptor.cc
@@ -57,33 +57,48 @@ void Acceptor::handleRead()
 {
   loop_->assertInLoopThread();
   InetAddress peerAddr;
-  //FIXME loop until no more
-  int connfd = acceptSocket_.accept(&peerAddr);
-  if (connfd >= 0)
+  int connfd;
+  int acceptCount = 0;
+
+  while (1)
   {
-    // string hostport = peerAddr.toIpPort();
-    // LOG_TRACE << "Accepts of " << hostport;
-    if (newConnectionCallback_)
+    connfd = acceptSocket_.accept(&peerAddr);
+    if (connfd >= 0)
     {
-      newConnectionCallback_(connfd, peerAddr);
+      // string hostport = peerAddr.toIpPort();
+      // LOG_TRACE << "Accepts of " << hostport;
+      acceptCount++;
+      if (newConnectionCallback_)
+      {
+        newConnectionCallback_(connfd, peerAddr);
+      }
+      else
+      {
+        sockets::close(connfd);
+      }
     }
     else
     {
-      sockets::close(connfd);
-    }
-  }
-  else
-  {
-    LOG_SYSERR << "in Acceptor::handleRead";
-    // Read the section named "The special problem of
-    // accept()ing when you can't" in libev's doc.
-    // By Marc Lehmann, author of livev.
-    if (errno == EMFILE)
-    {
-      ::close(idleFd_);
-      idleFd_ = ::accept(acceptSocket_.fd(), NULL, NULL);
-      ::close(idleFd_);
-      idleFd_ = ::open("/dev/null", O_RDONLY | O_CLOEXEC);
+      if (errno == EAGAIN || errno == EWOULDBLOCK)
+      {
+        LOG_TRACE << "Accept all connections acceptCount = " << acceptCount;
+        break;
+      }
+      if (errno == ECONNABORTED)
+      {
+        continue;
+      }
+      LOG_SYSERR << "in Acceptor::handleRead";
+      // Read the section named "The special problem of
+      // accept()ing when you can't" in libev's doc.
+      // By Marc Lehmann, author of livev.
+      if (errno == EMFILE)
+      {
+        ::close(idleFd_);
+        idleFd_ = ::accept(acceptSocket_.fd(), NULL, NULL);
+        ::close(idleFd_);
+        idleFd_ = ::open("/dev/null", O_RDONLY | O_CLOEXEC);
+      }
     }
   }
 }
