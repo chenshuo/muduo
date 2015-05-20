3b027512c901d652178e27d580bc1bb184377a62
alexstocks <alexstocks@foxmail.com>
2015-05-20 14:43:32 +0800

    change readme and todo & del cmake file

--- build.sh
+++ build.sh
@@ -1,25 +1,4 @@
 #!/bin/sh
 
-set -x
-
-SOURCE_DIR=`pwd`
-BUILD_DIR=${BUILD_DIR:-../build}
-BUILD_TYPE=${BUILD_TYPE:-release}
-INSTALL_DIR=${INSTALL_DIR:-../${BUILD_TYPE}-install}
-BUILD_NO_EXAMPLES=${BUILD_NO_EXAMPLES:-0}
-
-mkdir -p $BUILD_DIR/$BUILD_TYPE \
-  && cd $BUILD_DIR/$BUILD_TYPE \
-  && cmake \
-           -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-           -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
-           -DCMAKE_BUILD_NO_EXAMPLES=$BUILD_NO_EXAMPLES \
-           $SOURCE_DIR \
-  && make $*
-
-# Use the following command to run all the unit tests
-# at the dir $BUILD_DIR/$BUILD_TYPE :
-# CTEST_OUTPUT_ON_FAILURE=TRUE make test
-
-# cd $SOURCE_DIR && doxygen
-
+cd muduo/base/tests && make -f makefile && cd -
+cd muduo/net/tests && make -f makefile && cd -
