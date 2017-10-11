4dde0b35de6676eb2ce8c78063a783a727f404c3
pisa <paizzj@126.com>
2017-10-11 18:03:04 +0800

    add build dir for build

--- build.sh
+++ build.sh
@@ -1,4 +1,30 @@
 #!/bin/sh
 
-cd muduo/base/tests && make -f makefile && cd -
-cd muduo/net/tests && make -f makefile && cd -
+base=muduo/base/tests/lib
+net=muduo/net/tests/lib
+build=build
+
+# clean
+if [ "$1" = "clean" ]; then
+    echo "clean"
+
+    cd muduo/base/tests  && make -f makefile clean && cd -
+    cd muduo/net/tests  && make -f makefile clean && cd -
+    rm -rf $build
+    return
+fi
+
+# build
+cd muduo/base/tests  && make -f makefile && cd -
+cd muduo/net/tests  && make -f makefile && cd -
+
+
+# make build dir.
+if [ ! -d "build" ]; then
+    mkdir -p build/lib
+fi
+
+
+# copy the lib to build dir
+cp $base/libmuduo_base.a  $build/lib
+cp $net/libmuduo.a  $build/lib
