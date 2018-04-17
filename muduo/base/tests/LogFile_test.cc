962efae39e9fc3d9c8f2c66c4722f6dd829fdbee
Shuo Chen <chenshuo@chenshuo.com>
2018-04-17 00:58:10 -0700

    Fix warnings for gcc 8.
    Cherry-pick: a3327e24f43c9ef7bcd9a16235da1c779b2d111e

--- muduo/base/tests/LogFile_test.cc
+++ muduo/base/tests/LogFile_test.cc
@@ -17,8 +17,8 @@ void flushFunc()
 
 int main(int argc, char* argv[])
 {
-  char name[256];
-  strncpy(name, argv[0], 256);
+  char name[256] = { 0 };
+  strncpy(name, argv[0], sizeof name - 1);
   g_logFile.reset(new muduo::LogFile(::basename(name), 200*1000));
   muduo::Logger::setOutput(outputFunc);
   muduo::Logger::setFlush(flushFunc);
