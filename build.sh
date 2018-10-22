bc15dfbdd23a4ddeafa9f31b746c3dc7bcbaeaf9
Shuo Chen <chenshuo@chenshuo.com>
2018-10-21 22:59:06 -0700

    Simplify clang support.
    Cherry-pick: 2f6c9dcbe88439f8d1b922f9e0517de6613376e3

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ SOURCE_DIR=`pwd`
 BUILD_DIR=${BUILD_DIR:-../build}
 BUILD_TYPE=${BUILD_TYPE:-release}
 INSTALL_DIR=${INSTALL_DIR:-../${BUILD_TYPE}-install-cpp11}
-BUILD_NO_EXAMPLES=${BUILD_NO_EXAMPLES:-0}
+CXX=${CXX:-g++}
 
 ln -sf $BUILD_DIR/$BUILD_TYPE-cpp11/compile_commands.json
 
@@ -15,7 +15,6 @@ mkdir -p $BUILD_DIR/$BUILD_TYPE-cpp11 \
   && cmake \
            -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
            -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
-           -DMUDUO_BUILD_NO_EXAMPLES=$BUILD_NO_EXAMPLES \
            -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
            $SOURCE_DIR \
   && make $*
