2f6c9dcbe88439f8d1b922f9e0517de6613376e3
Shuo Chen <chenshuo@chenshuo.com>
2018-10-21 22:59:06 -0700

    Simplify clang support.

--- build.sh
+++ build.sh
@@ -6,14 +6,14 @@ SOURCE_DIR=`pwd`
 BUILD_DIR=${BUILD_DIR:-../build}
 BUILD_TYPE=${BUILD_TYPE:-release}
 INSTALL_DIR=${INSTALL_DIR:-../${BUILD_TYPE}-install}
-BUILD_NO_EXAMPLES=${BUILD_NO_EXAMPLES:-0}
+CXX=${CXX:-g++}
 
 mkdir -p $BUILD_DIR/$BUILD_TYPE \
   && cd $BUILD_DIR/$BUILD_TYPE \
   && cmake \
            -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
            -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
-           -DCMAKE_BUILD_NO_EXAMPLES=$BUILD_NO_EXAMPLES \
+           -DCMAKE_CXX_COMPILER=$CXX \
            $SOURCE_DIR \
   && make $*
 
