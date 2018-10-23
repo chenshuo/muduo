c9b046b7fbc32b2cdcd0086d9f927a8cff780adb
Shuo Chen <chenshuo@chenshuo.com>
2018-10-22 20:33:36 -0700

    v1.1.1

--- build.sh
+++ build.sh
@@ -8,12 +8,14 @@ BUILD_TYPE=${BUILD_TYPE:-release}
 INSTALL_DIR=${INSTALL_DIR:-../${BUILD_TYPE}-install}
 CXX=${CXX:-g++}
 
+ln -sf $BUILD_DIR/$BUILD_TYPE/compile_commands.json
+
 mkdir -p $BUILD_DIR/$BUILD_TYPE \
   && cd $BUILD_DIR/$BUILD_TYPE \
   && cmake \
            -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
            -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
-           -DCMAKE_CXX_COMPILER=$CXX \
+           -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
            $SOURCE_DIR \
   && make $*
 
