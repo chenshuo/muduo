c8b2cbc4d10c65bb74363c92f4dcd461ec96b76f
Shuo Chen <chenshuo@chenshuo.com>
2018-10-26 08:38:25 -0700

    Fix warnings in gcc 8 (#351).
    Cherry-pick: 373d2d127fc2f8e94afa3a24d52f9caa5b0fc89b

--- muduo/base/tests/AsyncLogging_test.cc
+++ muduo/base/tests/AsyncLogging_test.cc
@@ -53,7 +53,7 @@ int main(int argc, char* argv[])
 
   printf("pid = %d\n", getpid());
 
-  char name[256] = { 0 };
+  char name[256] = { '\0' };
   strncpy(name, argv[0], sizeof name - 1);
   muduo::AsyncLogging log(::basename(name), kRollSize);
   log.start();
