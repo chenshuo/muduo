1417ee111be1fe8a5cf4f3d90a23c5be1d7c875f
hfcrwx <hfcrwx@163.com>
2020-04-19 05:26:27 +0800

    minor fixes (#443)

--- muduo/base/Atomic.h
+++ muduo/base/Atomic.h
@@ -80,7 +80,7 @@ class AtomicIntegerT : noncopyable
 
   T getAndSet(T newValue)
   {
-    // in gcc >= 4.7: __atomic_exchange_n(&value, newValue, __ATOMIC_SEQ_CST)
+    // in gcc >= 4.7: __atomic_exchange_n(&value_, newValue, __ATOMIC_SEQ_CST)
     return __sync_lock_test_and_set(&value_, newValue);
   }
 
--- muduo/base/Timestamp.cc
+++ muduo/base/Timestamp.cc
@@ -24,7 +24,7 @@ string Timestamp::toString() const
   char buf[32] = {0};
   int64_t seconds = microSecondsSinceEpoch_ / kMicroSecondsPerSecond;
   int64_t microseconds = microSecondsSinceEpoch_ % kMicroSecondsPerSecond;
-  snprintf(buf, sizeof(buf)-1, "%" PRId64 ".%06" PRId64 "", seconds, microseconds);
+  snprintf(buf, sizeof(buf), "%" PRId64 ".%06" PRId64 "", seconds, microseconds);
   return buf;
 }
 
