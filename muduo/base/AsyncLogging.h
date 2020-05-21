8668a09bdb5584beaf07e5aab45683e62105e0a6
ggggitfkbjg <767105779@qq.com>
2020-05-21 11:36:49 +0800

    remove unused #include

--- muduo/base/AsyncLogging.h
+++ muduo/base/AsyncLogging.h
@@ -6,8 +6,6 @@
 #ifndef MUDUO_BASE_ASYNCLOGGING_H
 #define MUDUO_BASE_ASYNCLOGGING_H
 
-#include "muduo/base/BlockingQueue.h"
-#include "muduo/base/BoundedBlockingQueue.h"
 #include "muduo/base/CountDownLatch.h"
 #include "muduo/base/Mutex.h"
 #include "muduo/base/Thread.h"
