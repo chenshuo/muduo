78a3e36a3ac5711c2fe77d9f447863be41837f56
joker <cyp_fly@126.com>
2021-04-20 16:41:46 +0800

    write operation may fail, writtenBytes_ should be the actual number of bytes written

--- muduo/base/FileUtil.cc
+++ muduo/base/FileUtil.cc
@@ -31,25 +31,25 @@ FileUtil::AppendFile::~AppendFile()
 
 void FileUtil::AppendFile::append(const char* logline, const size_t len)
 {
-  size_t n = write(logline, len);
-  size_t remain = len - n;
-  while (remain > 0)
+  size_t written = 0;
+
+  while (written != len)
   {
-    size_t x = write(logline + n, remain);
-    if (x == 0)
+    size_t remain = len - written;
+    size_t n = write(logline + written, remain);
+    if (n != remain)
     {
       int err = ferror(fp_);
       if (err)
       {
         fprintf(stderr, "AppendFile::append() failed %s\n", strerror_tl(err));
+        break;
       }
-      break;
     }
-    n += x;
-    remain = len - n; // remain -= x
+    written += n;
   }
 
-  writtenBytes_ += len;
+  writtenBytes_ += written;
 }
 
 void FileUtil::AppendFile::flush()
